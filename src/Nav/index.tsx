22f8f879fb96f71e55e6f77852b5de402342e463
Chris <cgormley1275@gmail.com>
2024-12-09 17:21:00 -0500

    fixed navigation to maintain logged in user

--- src/Nav/index.tsx
+++ src/Nav/index.tsx
@@ -1,22 +1,37 @@
 import React from "react";
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Nav() {
     const { pathname } = useLocation();
-    return (
-        <ul className="nav nav-pills">
-            <li className="nav-item">
-                <a href="/">Home</a>
-            </li>
-            <li className="nav-item">
-                <a href="/search">Search</a>
-            </li>
-            <li className="nav-item">
-                <a href="/profile">Profile</a>
-            </li>
-            <li className="nav-item">
-                <a href="/signin">Sign In</a>
-            </li>
-        </ul>
+    // return (
+    //     <ul className="nav nav-pills">
+    //         <li className="nav-item">
+    //             <a href="/">Home</a>
+    //         </li>
+    //         <li className="nav-item">
+    //             <a href="/search">Search</a>
+    //         </li>
+    //         <li className="nav-item">
+    //             <a href="/profile">Profile</a>
+    //         </li>
+    //         <li className="nav-item">
+    //             <a href="/signin">Sign In</a>
+    //         </li>
+    //     </ul>
+    // )
+    const links = [
+        { label: "Home", path: "/"},
+        { label: "Profile", path: "/profile" },
+        { label: "Search", path: "/search"  },
+        { label: "Sign In", path: "/signin" }
+    ]
+    return(
+        <div>
+            {links.map((link) => (
+                <Link key={link.path} to={link.path}>
+                    {link.label}
+                </Link>
+            ))}
+        </div>
     )
 }
