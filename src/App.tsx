5fc1d542cc2e9975d4a24a281e86bd20f03c7246
Chris <cgormley1275@gmail.com>
2024-12-09 12:13:01 -0500

    restarting project

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home/index.tsx";
+import SignIn from './SignIn/index.tsx';
+import SignUp from './SignUp/index.tsx';
+import Search from './Search/index.tsx';
+import Profile from './Profile/index.tsx';
+import SearchDetails from './Search/Details/index.tsx';
+import SearchBarResults from './Search/SearchBarResults/index.tsx';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin/" element={<SignIn />} />
+        <Route path="/signup/" element={<SignUp />} />
+        <Route path="/search/:query" element={<SearchBarResults />} />
+        <Route path="/search/" element={<Search />} />
+        <Route path="/details/:airportCode" element={<SearchDetails />} />
+        <Route path="/profile/" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Search/SearchBarResults/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SearchBarResults() {
+    return <div>Search Bar Results</div>
+}
